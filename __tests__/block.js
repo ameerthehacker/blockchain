f2bde082726198fa4e74d3b0ee4707c2189806c3
ameerthehacker <ameerjhanprof@gmail.com>
2018-04-06 19:05:00 +0530

    Add tests for Block using jest :fire:

---
+++ __tests__/block.js
@@ -0,0 +1,29 @@
+const Block = require("../src/block");
+const SHA256 = require("crypto-js/sha256");
+
+describe("Block", () => {
+  let lastBlock, data, block;
+
+  beforeEach(() => {
+    data = "foo";
+    lastBlock = Block.genesis();
+    block = Block.mineBlock(lastBlock, data);
+  });
+
+  it("should have `data` given as input", () => {
+    expect(block.data).toBe(data);
+  });
+
+  it("shoud set the `lastHash` value correctly", () => {
+    expect(block.lastHash).toBe(lastBlock.hash);
+  });
+
+  it("should calculate the `hash` correctly", () => {
+    const timestamp = block.timestamp;
+    const lastHash = block.lastHash;
+    const data = block.data;
+    const hash = SHA256(`${timestamp}${lastHash}${data}`).toString();
+
+    expect(block.hash).toBe(hash);
+  });
+});
